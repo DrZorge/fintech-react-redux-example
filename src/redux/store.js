bbf0fbcaee475c568136a5173355d8d1307fd14d
DrZorge <vladonxp@mail.ru>
2019-04-14 10:42:48 +0300

    add action types, small refactoring

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,10 +2,10 @@ import { createStore } from "redux";
 
 import todoReducer from "./todoReducer";
 
-const initialState = [
+const preloadedState = [
   { id: 1, title: "Выпить кофе", done: false },
   { id: 2, title: "Изучить HTML, CSS", done: false },
   { id: 3, title: "Изучить JS", done: false }
 ];
 
-export default createStore(todoReducer, initialState);
+export default createStore(todoReducer, preloadedState);
--- src/redux/todoReducer.js
+++ src/redux/todoReducer.js
@@ -1,12 +1,14 @@
+import { ADD_TODO, REMOVE_TODO, TOGGLE_TODO } from "./actionTypes";
+
 export default (state, action) => {
   switch (action.type) {
-    case "ADD_TODO":
+    case ADD_TODO:
       return [...state, action.payload];
 
-    case "REMOVE_TODO":
+    case REMOVE_TODO:
       return state.filter(item => item.id !== action.payload.id);
 
-    case "TOGGLE_TODO":
+    case TOGGLE_TODO:
       return state.map(item => {
         if (item.id === action.payload.id) {
           return { ...item, done: !item.done };
